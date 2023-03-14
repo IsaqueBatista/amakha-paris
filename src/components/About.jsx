0d2737f7b3b8a9e9cb148fac1ed254c7072f157d
isaquebatista <isaquebatista1716@gmail.com>
2023-03-14 19:42:10 -0300

    Adicionando vídeo - Component About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,9 +6,9 @@ const About = () => {
   return (
     <div className="w-full text-center">
       <div className="w-full h-full p-2 px-10 bg-gray-700">
-        <div className="bg-gradient-to-t from-gray-600 to-gray-800 rounded-md">
+        <div className=" bg-gradient-to-t from-gray-700 to-slate-800 rounded-md">
           <div className="m-auto">
-            <h1 className="text-white py-8 mt-20">Quem somos</h1>
+            <h1 className="text-slate-100 py-8 mt-20">Quem somos</h1>
             <p className="p-5 text-white text-center m-auto px-20">
               A Amakha Paris é uma das maiores marcas de beleza e bem-estar do
               Brasil. A mesma acredita no empreendedorismo social e é a
@@ -16,7 +16,18 @@ const About = () => {
               mais de 500 itens de alta qualidade em seu portfólio.
             </p>
           </div>
-
+          <div className="flex justify-center mt-5">
+            <iframe
+            className="shadow-md shadow-gray-300 rounded-md"
+              width="560"
+              height="315"
+              src="https://www.youtube-nocookie.com/embed/YzQ_eZx_KM4"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </div>
           <div className="flex md:flex-row-reverse flex-col pr-5">
             <div className="flex md:flex items-center gap-10 justify-center">
               <Image src={Pa} width="200" height="180" alt="/" />
