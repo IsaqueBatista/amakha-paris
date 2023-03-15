9f3a29b96caa9e71d6d8d169f79f014c5c26d349
isaquebatista <isaquebatista1716@gmail.com>
2023-03-15 14:51:17 -0300

    Styles Catalog Component

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -6,11 +6,11 @@ import CatalogImg from "../../public/assets/Catalog/catalogo.jpg";
 const Catalog = () => {
   return (
     <div>
-      <div className="mb-2">
+      <div className="p-2 text-white bg-gradient-to-t from-zinc-900 via-gray-600 to-zinc-900">
         <p>Clique na imagem e faça o download do catálogo.</p>
       </div>
 
-      <div className="flex justify-center px-1 mb-10">
+      <div className="flex justify-center px-1 pb-10 bg-gradient-to-t from-zinc-900 via-gray-600 to-zinc-900">
         <Link href="/">
           <Image
             className="rounded-sm border-solid border-2 border-black"
