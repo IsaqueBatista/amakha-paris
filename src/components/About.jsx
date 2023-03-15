8fea19e966da7e550ad8cfa315a7b54ff170c0e9
isaquebatista <isaquebatista1716@gmail.com>
2023-03-15 14:31:04 -0300

    Styles Component About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,11 +5,11 @@ import Pa from "../../public/assets/About/logo-pa.png";
 const About = () => {
   return (
     <div className="w-full text-center">
-      <div className="w-full h-full p-2 px-10 bg-gray-700">
-        <div className=" bg-gradient-to-t from-gray-700 to-slate-800 rounded-md">
+      <div className="w-full h-full p-2 px-10 bg-gradient-to-t from-gray-700 via-gray-500 to-zinc-300">
+        <div className="bg-gradient-to-t from-zinc-900 via-gray-600 to-zinc-900 rounded-md">
           <div className="m-auto">
-            <h1 className="text-slate-100 py-8 mt-20">Quem somos</h1>
-            <p className="p-5 text-white text-center m-auto px-20">
+            <h1 className="text-white py-8 mt-20">Quem somos</h1>
+            <p className="p-5 text-white text-center text-lg m-auto px-20">
               A Amakha Paris é uma das maiores marcas de beleza e bem-estar do
               Brasil. A mesma acredita no empreendedorismo social e é a
               oportunidade de negócios para mais de 1 milhão de consultores. São
@@ -33,7 +33,7 @@ const About = () => {
               <Image src={Pa} width="200" height="180" alt="/" />
             </div>
 
-            <p className="p-5 text-white text-center m-auto px-20 pr-10">
+            <p className="p-5 text-white text-center text-lg m-auto px-20 pr-10">
               Em todo o Brasil, você encontra Pontos de Apoio para adquirir os
               produtos. E você também pode ser o empreendedor de um deles.
             </p>
