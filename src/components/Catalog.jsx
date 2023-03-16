27e1d7aae08a49f06b9b74d553bb1967981c10e7
isaquebatista <isaquebatista1716@gmail.com>
2023-03-16 17:53:38 -0300

    Alteração catálogo

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -5,7 +5,6 @@ import CatalogImg from "../../public/assets/Catalog/catalogo.jpg";
 const Catalog = () => {
   return (
     <div id="catalogo">
-
       <div className="flex justify-center px-1 pt-10 bg-gradient-to-t from-zinc-900 via-gray-600 to-zinc-900">
         <Image
           className="rounded-sm border-solid border-2 border-black"
@@ -17,15 +16,15 @@ const Catalog = () => {
       </div>
 
       <div className="p-2 text-white bg-gradient-to-t from-zinc-900 via-gray-600 to-zinc-900">
-        <p className="underline mb-5">
-          <a
-            href="../../public/assets/Catalogo_AMAKHA.pdf"
-            type="application/pdf"
-            download="Catalogo_AMAKHA.pdf"
-          >
+        <a
+          href="../../public/assets/Catalogo_AMAKHA.pdf"
+          download="Catalogo_AMAKHA.pdf"
+          type="application/pdf"
+        >
+          <p className="underline mb-5">
             Clique aqui e faça o download do catálogo.
-          </a>
-        </p>
+          </p>
+        </a>
       </div>
     </div>
   );
