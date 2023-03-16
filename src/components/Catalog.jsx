dfb7adb25abe7627abd85e7dd2d322eea7fd941a
isaquebatista <isaquebatista1716@gmail.com>
2023-03-16 17:31:55 -0300

    Testes download catálogo

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -5,20 +5,27 @@ import CatalogImg from "../../public/assets/Catalog/catalogo.jpg";
 const Catalog = () => {
   return (
     <div id="catalogo">
-      <div className="p-2 text-white bg-gradient-to-t from-zinc-900 via-gray-600 to-zinc-900">
-        <p>Clique na imagem e faça o download do catálogo.</p>
+
+      <div className="flex justify-center px-1 pt-10 bg-gradient-to-t from-zinc-900 via-gray-600 to-zinc-900">
+        <Image
+          className="rounded-sm border-solid border-2 border-black"
+          src={CatalogImg}
+          width="100%"
+          height="800"
+          alt="Catálogo Amakha Paris"
+        />
       </div>
 
-      <div className="flex justify-center px-1 pb-10 bg-gradient-to-t from-zinc-900 via-gray-600 to-zinc-900">
-        <a href="../../public/assets/Catalogo_AMAKHA.pdf" type="application/pdf" download >
-          <Image
-            className="rounded-sm border-solid border-2 border-black cursor-pointer"
-            src={CatalogImg}
-            width="100%"
-            height="800"
-            alt="Catálogo Amakha Paris"
-          />
-        </a>
+      <div className="p-2 text-white bg-gradient-to-t from-zinc-900 via-gray-600 to-zinc-900">
+        <p className="underline mb-5">
+          <a
+            href="../../public/assets/Catalogo_AMAKHA.pdf"
+            type="application/pdf"
+            download="Catalogo_AMAKHA.pdf"
+          >
+            Clique aqui e faça o download do catálogo.
+          </a>
+        </p>
       </div>
     </div>
   );
