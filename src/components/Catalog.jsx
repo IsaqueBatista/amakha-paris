6b6d696906a1b67b25384031f6e11a44db4dfcf3
isaquebatista <isaquebatista1716@gmail.com>
2023-03-16 10:49:47 -0300

    Adicionando PDF Catalog Component

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -4,13 +4,13 @@ import CatalogImg from "../../public/assets/Catalog/catalogo.jpg";
 
 const Catalog = () => {
   return (
-    <div id="catalogo" className="w-full">
+    <div id="catalogo">
       <div className="p-2 text-white bg-gradient-to-t from-zinc-900 via-gray-600 to-zinc-900">
         <p>Clique na imagem e faça o download do catálogo.</p>
       </div>
 
       <div className="flex justify-center px-1 pb-10 bg-gradient-to-t from-zinc-900 via-gray-600 to-zinc-900">
-        <a>
+        <a download type="application/pdf" href="./assets/Catalogo-AMAKHA.pdf">
           <Image
             className="rounded-sm border-solid border-2 border-black cursor-pointer"
             src={CatalogImg}
