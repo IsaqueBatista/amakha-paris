ec07e01e615448315fda3507718297e7bd76aacd
isaquebatista <isaquebatista1716@gmail.com>
2023-03-17 20:49:17 -0300

    Comentário opção download link mediafire

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -16,6 +16,9 @@ const Catalog = () => {
       </div>
 
       <div className="p-2 text-white bg-gradient-to-t from-[#384252] via-zinc-900 to-orange-700">
+        {/* <a
+          href="https://download1519.mediafire.com/hi7983xrkxwgUE-ecc9mIr5lRoO0XDAuxEMUGvwUwuJW-iuIhW_pa0QK1knXgFL_mkrlz7TyTvPJ_d5Dw116heuPeUNn61Q/hb4rmg833rkzbdi/Catalogo_AMAKHA.pdf"
+        > */}
         <a
           href="../../public/assets/Catalogo_AMAKHA.pdf"
           download="Catalogo_AMAKHA.pdf"
