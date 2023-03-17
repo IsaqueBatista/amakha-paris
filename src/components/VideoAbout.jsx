bfe77b05c8f441b265fdf5fe88743f2b544c8456
isaquebatista <isaquebatista1716@gmail.com>
2023-03-17 20:36:39 -0300

    Alterando background VideoAbout component

--- src/components/VideoAbout.jsx
+++ src/components/VideoAbout.jsx
@@ -4,10 +4,10 @@ import React from "react";
 const VideoAbout = () => {
   return (
     <div className="w-full text-center">
-      <div className="w-full h-full p-2 px-10 bg-gradient-to-t from-zinc-300 to-slate-200">
+      <div className="w-full h-full p-2 px-10 bg-gradient-to-t from-gray-700 via-purple-700 to-slate-200">
         <div>
 
-          <div className="flex justify-center my-20">
+          <div className="flex justify-center mt-5 mb-10">
             <iframe
               className="border-solid border-2 border-black"
               width="560"
