40894fab4adfc7b57b3fbc05ba4afc857c5fc96e
isaquebatista <isaquebatista1716@gmail.com>
2023-03-19 21:07:39 -0300

    Alinhamento de texto About Component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,19 +11,19 @@ const About = () => {
       <div className="absolute bg-black/10 z-[2] p-10" />
       <div className="p-5 bg-black/90 text-white z-[2] mt-[+8rem] font-serif">
         <h1 className="text-4xl uppercase">Quem somos</h1>
-        <p className="py-5 text-lg font-serif">
+        <p className="py-5 px-3 md:text-justify text-center text-lg font-serif md:px-20">
           A Amakha Paris é uma das maiores marcas de beleza e bem-estar do
           Brasil. A mesma acredita no empreendedorismo social e é a oportunidade
           de negócios para mais de 1 milhão de consultores. São mais de 500
           itens de alta qualidade em seu portfólio.
         </p>
 
-         <div className="flex md:flex-row-reverse flex-col pr-5">
+         <div className="flex md:flex-row-reverse flex-col md:pr-5">
           <div className="flex items-center gap-10 justify-center">
             <Image src={Pa} width="200" height="180" alt="/" />
           </div>
 
-          <p className="p-5 text-white text-center text-lg m-auto px-20 pr-10 ">
+          <p className="p-5 px-3 text-white md:text-justify text-center m-auto md:px-20 md:pr-10 py-5 text-lg font-serif">
             Em todo o Brasil, você encontra Pontos de Apoio para adquirir os
             produtos. E você também pode ser o empreendedor de um deles.
           </p>
