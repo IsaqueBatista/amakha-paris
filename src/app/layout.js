1d0d476785f47d200035a94dd224ee987ec4f16a
isaquebatista <isaquebatista1716@gmail.com>
2023-03-24 14:39:10 -0300

    Adicionando favicon no site

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,17 @@
 import "../styles/globals.css";
 
 export const metadata = {
-  title: 'Amakha Paris Cadastro',
-  description: 'Amakha Paris',
-}
+  title: "Amakha Paris Cadastro",
+  description: "Amakha Paris",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
     </html>
-  )
+  );
 }
-  
