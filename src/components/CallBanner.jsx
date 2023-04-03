d29415acdd62334b12aff9e44dcedc19078f08fd
isaquebatista <isaquebatista1716@gmail.com>
2023-04-03 19:14:12 -0300

    Alterando cor do Background Component CallBanner

--- src/components/CallBanner.jsx
+++ src/components/CallBanner.jsx
@@ -5,7 +5,7 @@ import Banner from "../../public/assets/Banner/banner-chamada.jpg";
 
 const CallBanner = () => {
   return (
-    <div className="py-3 m-auto flex justify-center bg-gradient-to-t from-gray-400 to-gray-700">
+    <div className="py-3 m-auto flex justify-center bg-gradient-to-t from-[#f3ac3b] to-[#f7cf8f]">
       <div className="m-auto flex justify-center pb-5">
         <Link
           target={"_blank"}
