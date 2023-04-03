756a81f4969cb0f48af0e42a2e2293e73b4535e6
isaquebatista <isaquebatista1716@gmail.com>
2023-04-03 19:15:52 -0300

    Atualizando Component VideoAbout com novo video, background e cor dos botões

--- src/components/VideoAbout.jsx
+++ src/components/VideoAbout.jsx
@@ -4,29 +4,30 @@ import { SiWhatsapp } from "react-icons/si";
 const VideoAbout = () => {
   return (
     <div className="w-full text-center">
-      <div className="w-full h-full p-2 px-10 bg-gradient-to-t from-gray-700 via-purple-700 to-gray-700">
+      <div className="w-full h-full p-2 px-10 custom-img2 items-center">
         <div>
-          <div className="flex justify-center mt-5 mb-10">
+          <div className="flex justify-center mt-5 mb-10 ">
             <iframe
-              className="border-solid border-2 border-black"
-              width="560"
-              height="315"
-              src="https://www.youtube-nocookie.com/embed/YzQ_eZx_KM4"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen
-            ></iframe>
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/WbXDcOhDRk4" 
+            title="YouTube video player" 
+            frameborder="0" 
+            allow="accelerometer; 
+            autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            allowfullscreen>
+              
+            </iframe>
           </div>
         </div>
-        <div className=" pb-2 m-auto flex justify-center gap-2">
-          <div className="gap-2 p-2 items-center justify-center md:flex md:gap-4">
+        <div className=" p-2 m-auto flex justify-center gap-2">
+          <div className="gap-2 p-3 items-center justify-center md:flex md:gap-4 bg-zinc-50">
             <a
               href="https://wa.me/5534992225485?text=Oi%20Isaque,%20eu%20gostaria%20de%20saber%20mais%20informa%C3%A7%C3%B5es%20para%20revender%20Amakha%20Paris."
               target="_blank"
               rel="noreferrer"
             >
-              <button className="text-white hover:bg-[#19e065] bg-[#0bb34b] shadow-md shadow-gray-800 rounded-2xl py-2 flex items-center gap-1 px-4">
+              <button className="tracking-wide mx-auto text-white bg-[#FFAC00] hover:bg-[#bf6c07] shadow-md shadow-gray-800 rounded-2xl py-2 flex items-center gap-1 px-4">
                 <SiWhatsapp className="text-xl" />
                 Quero revender!
               </button>
@@ -37,7 +38,7 @@ const VideoAbout = () => {
             target="_blank"
             rel="noreferrer"
           >
-            <button className="text-white hover:bg-[#19e065] bg-[#0bb34b] shadow-md shadow-gray-800 rounded-2xl py-2 flex items-center gap-1 px-4">
+            <button className="tracking-wide mx-auto text-white bg-[#FFAC00] shadow-md shadow-gray-800 rounded-2xl py-2 flex items-center gap-1 px-4">
               <SiWhatsapp className="text-xl" />
               Quero comprar com 50% de desconto!
             </button>
